b4954816c3579e687bd6877574bb878f6c57a29d
loubnabaroudi <154988277+loubnabaroudi@users.noreply.github.com>
2024-04-19 20:44:29 +0100

    Add files via upload

---
+++ tp1-partie1/src/pres/Presentation.java
@@ -0,0 +1,20 @@
+package pres;
+
+import dao.DaoImpl;
+import ext.DaoImpl2;
+import metier.MetierImpl;
+
+public class Presentation {
+
+    public static void main(String[] args){
+
+        /*
+        injection des dependances par instanciation
+        dynamique => new
+         */
+        DaoImpl2 dao = new DaoImpl2();
+        MetierImpl metier = new MetierImpl();
+        metier.setDao(dao);
+        System.out.println("resultat = " + metier.calcul());
+    }
+}
