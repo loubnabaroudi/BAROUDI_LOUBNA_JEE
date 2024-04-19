c19c76917e7c5e2c96892ba8e2e661ece19649b1
loubnabaroudi <154988277+loubnabaroudi@users.noreply.github.com>
2024-04-19 21:17:04 +0100

    Add files via upload

---
+++ TP1/tp1-partie2/src/main/java/dao/DaoImpl.java
@@ -0,0 +1,17 @@
+package dao;
+
+import org.springframework.stereotype.Component;
+
+@Component("dao")
+public class DaoImpl implements IDao{
+    @Override
+    public double getData() {
+        /*
+        connecter a la bd pour recuperer la temp
+         */
+
+        System.out.println("version base de donnees");
+        double temp = Math.random()*40 ;
+        return temp;
+    }
+}
---
+++ TP1/tp1-partie2/src/main/java/pres/Presentation.java
@@ -0,0 +1,19 @@
+package pres;
+
+import ext.DaoImpl2;
+import metier.MetierImpl;
+
+public class Presentation {
+
+    public static void main(String[] args){
+
+        /*
+        injection des dependances par instanciation
+        dynamique => new
+         */
+        DaoImpl2 dao = new DaoImpl2();
+        MetierImpl metier = new MetierImpl();
+        //metier.setDao(dao);
+        System.out.println("resultat = " + metier.calcul());
+    }
+}
