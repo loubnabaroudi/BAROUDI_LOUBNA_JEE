cfa076d014de3669501993110365d77064ad0d30
loubnabaroudi <154988277+loubnabaroudi@users.noreply.github.com>
2024-04-20 22:04:17 +0100

    Add files via upload

---
+++ TP3/hopitalll/src/main/java/ma/fsm/hopitalll/repository/PatientRepository.java
@@ -0,0 +1,17 @@
+package ma.fsm.hopitalll.repository;
+
+import ma.fsm.hopitalll.entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Controller;
+
+public interface PatientRepository extends JpaRepository<Patient,Long> {
+    Page<Patient> findByNomContains(String keyword, Pageable pageable);
+    @Query("select p from Patient p where p.nom like:x")
+    Page<Patient> chercher(@Param("x") String keyword,Pageable pageable);
+
+
+}
