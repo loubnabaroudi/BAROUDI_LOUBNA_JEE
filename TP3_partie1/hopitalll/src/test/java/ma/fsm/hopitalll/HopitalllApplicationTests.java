ed6bdeec103117bee9d276a9272aba27ad3709ce
loubnabaroudi <154988277+loubnabaroudi@users.noreply.github.com>
2024-05-12 16:18:58 +0100

    Add files via upload

---
+++ TP3_partie1/hopitalll/src/test/java/ma/fsm/hopitalll/HopitalllApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.fsm.hopitalll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopitalllApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
